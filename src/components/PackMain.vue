1f2af88406da92ab9b44d455f242233a56e23356
flame <yaolynzc@126.com>
2018-04-08 17:46:42 +0800

    vue纯前端demo

---
+++ src/components/PackMain.vue
@@ -0,0 +1,119 @@
+<template>
+  <el-container>
+    <aside>
+      <el-form :model="numberValidateForm" ref="numberValidateForm" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="编码：" prop="age">
+          <el-input type="age" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('numberValidateForm')">入库</el-button>
+          <el-button @click="resetForm('numberValidateForm')">清空</el-button>
+        </el-form-item>
+      </el-form>
+    </aside>
+    <el-container>
+    <el-main>
+      <div class="search-params-block">
+        入库日期：
+        <el-input
+          placeholder="请选择日期"
+          prefix-icon="el-icon-date"
+          v-model="input2">
+        </el-input>
+        包裹编号：
+        <el-input
+          placeholder="请输入编号"
+          prefix-icon="el-icon-search"
+          v-model="input21">
+        </el-input>
+        <el-button type="primary" icon="el-icon-search">搜索</el-button>
+      </div>
+      <el-table
+        :data="tableData"
+        style="width: 100%">
+        <el-table-column
+          label="日期"
+          width="180">
+          <template slot-scope="scope">
+            <i class="el-icon-time"></i>
+            <span style="margin-left: 10px">{{ scope.row.date }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="姓名"
+          width="180">
+          <template slot-scope="scope">
+            <el-popover trigger="hover" placement="top">
+              <p>姓名: {{ scope.row.name }}</p>
+              <p>住址: {{ scope.row.address }}</p>
+              <div slot="reference" class="name-wrapper">
+                <el-tag size="medium">{{ scope.row.name }}</el-tag>
+              </div>
+            </el-popover>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'PackMain',
+  data () {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }],
+      input2: '',
+      input21: ''
+    }
+  },
+  methods: {
+    handleEdit (index, row) {
+      console.log(index, row)
+    },
+    handleDelete (index, row) {
+      console.log(index, row)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .el-input {
+    width: 200px;
+  }
+  .search-params-block {
+    float: left;
+  }
+  .el-table {
+    margin-top: 80px;
+  }
+</style>
